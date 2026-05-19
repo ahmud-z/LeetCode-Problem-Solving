cad82a3c21344019ae483fb21b940198f07cabea
Ahmudul Hossain <104680301+ahmud-z@users.noreply.github.com>
2026-05-19 12:28:55 +0600

    solved

---
+++ 412_fizz-buzz.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+    let i = 1;
+    let result = []
+
+    while (true) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz")
+        }
+        else if (i % 3 === 0) {
+            result.push("Fizz");
+        }
+        else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(String(i));
+        }
+
+        if (i == n) {
+            break
+        }
+
+        i++;
+    }
+
+    return result;
+};
